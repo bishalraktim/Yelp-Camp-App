d45900639dc42962cbbc819c25e3e6c253c293ab
bishalraktim <raktimbishal@gmail.com>
2020-03-26 10:08:48 +0000

    Connect MongoDB Atlas

--- app.js
+++ app.js
@@ -18,8 +18,9 @@ var commentRoutes 		= require("./routes/comments"),
 	campgroundRoutes 	= require("./routes/campgrounds"),
 	indexRoutes 		= require("./routes/index")
 
-mongoose.connect("mongodb://localhost/yelp_camp_v20", 
-				 {useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false, useCreateIndex: true});
+mongoose.connect(process.env.CONNECT_DB, 
+				 {useUnifiedTopology: true, useNewUrlParser: true, 
+				  useFindAndModify: false, useCreateIndex: true});
 
 
 app.set("view engine", "ejs");
