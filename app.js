f5438d8574aabf64f972e886377d31f17ef8d508
bishalraktim <raktimbishal@gmail.com>
2020-03-26 12:12:15 +0000

    Fix deployment errors

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
 	console.log("The YelpCamp Server Has Started!");
 });
 
