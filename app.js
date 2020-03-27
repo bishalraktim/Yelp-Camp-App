8fcf93907aac113d93cfd415b09933672f31e431
bishalraktim <raktimbishal@gmail.com>
2020-03-27 22:50:32 +0000

    Fix deployment errors

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ var commentRoutes 		= require("./routes/comments"),
 	campgroundRoutes 	= require("./routes/campgrounds"),
 	indexRoutes 		= require("./routes/index")
 
-mongoose.connect("mongodb+srv://bishal:RaKtim@20521101@yelp-camp-cluster-hpiyp.mongodb.net/test?retryWrites=true&w=majority", 
+mongoose.connect("mongodb+srv://" + process.env.CONNECT_DB + "@yelp-camp-cluster-hpiyp.mongodb.net/test?retryWrites=true&w=majority", 
 				 {useUnifiedTopology: true, useNewUrlParser: true, 
 				  useFindAndModify: false, useCreateIndex: true});
 
